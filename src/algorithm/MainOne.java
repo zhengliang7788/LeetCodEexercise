cf370f180dbb5fe8981c6eeab84430ce5d693dfa
zhengliang <zhengliang@yonyou.com>
2023-03-29 16:58:15 +0800

    fix:第一题两数之和

---
+++ src/algorithm/MainOne.java
@@ -0,0 +1,40 @@
+package algorithm;
+
+import java.util.Arrays;
+
+/**
+ * 1.两数之和
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * <p>
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ */
+public class MainOne {
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] arr = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    arr[0] = i;
+                    arr[1] = j;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(MainOne.twoSum(arr, target)));
+
+        int[] nums = {3, 2, 4};
+        int target2 = 6;
+
+        System.out.println(Arrays.toString(MainOne.twoSum(nums, target2)));
+
+    }
+}
