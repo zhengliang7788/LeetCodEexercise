db9e394e60361453d15fe24d2efadc2977a36456
zhengliang <zhengliang@yonyou.com>
2023-04-17 17:26:49 +0800

    fix:LeetCode

---
+++ src/algorithm/Main13.java
@@ -0,0 +1,99 @@
+package algorithm;
+
+/**
+ * 罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。
+ *
+ * 字符          数值
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * 例如， 罗马数字 2 写做 II ，即为两个并列的 1 。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+ *
+ * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+ *
+ * I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+ * X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。
+ * C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+ * 给定一个罗马数字，将其转换成整数。
+ *
+ * 示例 1:
+ *
+ * 输入: s = "III"
+ * 输出: 3
+ * 示例 2:
+ *
+ * 输入: s = "IV"
+ * 输出: 4
+ * 示例 3:
+ *
+ * 输入: s = "IX"
+ * 输出: 9
+ * 示例 4:
+ *
+ * 输入: s = "LVIII"
+ * 输出: 58
+ * 解释: L = 50, V= 5, III = 3.
+ */
+public class Main13 {
+
+    public static int romanToInt(String s) {
+
+        int sum = 0;
+
+        int length = s.length();
+
+        int maxLevel = 0;
+
+        // 从右往左相加 如果 右边的值大于左边的值 则减去左边的值  ‘XIV’
+        for (int i = length - 1; i >= 0; i--)
+        {
+            int value = getValue(s.charAt(i));
+
+            if (value >= maxLevel)
+            {
+                sum += value;
+
+                maxLevel = value;
+            }
+            else
+            {
+                sum -= value;
+            }
+
+        }
+
+        return sum;
+
+    }
+
+    public static int getValue(char a) {
+        switch (a) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Main13.romanToInt("IV"));
+    }
+
+
+}
